7310616e952e4e9a01b3129716e5877f603ff2b0
riflemin@ksr.ch <riflemin@ksr.ch>
2023-05-06 08:00:57 +0200

    added asteroids_v05.py

---
+++ asteroids_v05.py
@@ -0,0 +1,19 @@
+
+
+
+
+while True:
+    update(x1,y1,s1,l_u1)
+    update(x2,y1,s1,l_u1)
+    update(x3,y1,s1,l_u1)
+    update(x4,y1,s1,l_u1)
+
+
+
+def update():
+    if asteroid is here:
+        y += 1
+        ...
+    else:
+        y = 0
+        x = random
