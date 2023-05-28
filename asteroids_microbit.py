5f2a653b6e138c6f52f2440bb9167b1c8428d30e
riflemin@ksr.ch <riflemin@ksr.ch>
2023-05-28 07:26:51 +0200

    don't know if it works

--- asteroids_microbit.py
+++ asteroids_microbit.py
@@ -1,8 +1,27 @@
 from microbit import *
 import music
-from asteroids_models.py import Game
+from asteroids_model.py import Game
 
 DIS_x = 5
 DIS_y = 5
+PLAYER_BRIGHTNESS = 9
+ASTEROID_BRIGHTNESS = 5
 
-Game(DIS_x, DIS_y)
+game = Game(DIS_x, DIS_y)
+
+while not game.player_is_colliding():
+    # model
+    game.get_time_in_ms()
+    game.spawn_asteroids()
+    game.update_asteroids()
+    game.player.update()
+
+    #view
+    # display asteroid
+    for asteroid in game.asteroids:
+        display.set_pixel(asteroid.x, asteroid.y, ASTEROID_BRIGHTNESS)
+    display.set_pixel
+    # display player
+    display.set_pixel(game.player.x, game.player.y, PLAYER_BRIGHTNESS)
+    
+
